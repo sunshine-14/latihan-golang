9b1e8d3700f6be030753683e0391c92630ca15fc
Owen <foxtrot.dev@nexa.net.id>
2025-08-11 16:35:54 +0700

    update log url start'

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	routes.CustomerRoutes()
 	routes.UserRoutes()
 
-	fmt.Println("Server jalan di http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	addr := ":8080"
+	fmt.Println("Server jalan di", addr)
+	http.ListenAndServe(addr, nil)
 }
